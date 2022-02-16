eea4443d59894d50d0edb9b23da6bf583abcda39
Fernando Castillo Torrico <fernandocto.scz@gmail.com>
2022-02-16 19:58:25 -0400

    NEW: add client service

---
+++ caso-parqueo/src/app/shared/services/client.service.ts
@@ -0,0 +1,34 @@
+import { Client } from './../types/Client';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Array<Client>> {
+    return this.http.get<Array<Client>>(`${environment.endpoints}/client`);
+  }
+
+  getOne(id: number): Observable<Array<Client>> {
+    return this.http.get<Array<Client>>(`${environment.endpoints}/client/${id}`);
+  }
+
+  create(data: Client): Observable<null> {
+    return this.http.post<null>(`${environment.endpoints}/client`, data);
+  }
+
+  update(id: number, data: Partial<Client>): Observable<null> {
+    return this.http.patch<null>(`${environment.endpoints}/client/${id}`, data);
+  }
+
+  delete(id: number): Observable<Array<Client>> {
+    return this.http.delete<Array<Client>>(`${environment.endpoints}/client/${id}`);
+  }
+}
---
+++ caso-parqueo/src/app/shared/types/Client.ts
@@ -0,0 +1,10 @@
+export interface Client {
+  id: number;
+  name: string
+  surnames: string
+  age: number,
+  phone: number,
+  email: string|null,
+  createdAt: Date,
+  updatedAt: Date
+}
