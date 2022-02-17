badc4afef5eaa83c383ab6bfec236ce7dba6c212
Fernando Castillo Torrico <fernandocto.scz@gmail.com>
2022-02-17 17:56:29 -0400

    NEW: add register service

---
+++ caso-parqueo/src/app/shared/services/register.service.ts
@@ -0,0 +1,34 @@
+import { Register, RegisterDTO } from './../types/Register';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Array<Register>> {
+    return this.http.get<Array<Register>>(`${environment.endpoints}/register`);
+  }
+
+  getOne(id: number): Observable<Register> {
+    return this.http.get<Register>(`${environment.endpoints}/register/${id}`);
+  }
+
+  create(data: RegisterDTO): Observable<null> {
+    return this.http.post<null>(`${environment.endpoints}/register`, data);
+  }
+
+  update(id: number, data: Partial<RegisterDTO>): Observable<null> {
+    return this.http.patch<null>(`${environment.endpoints}/register/${id}`, data);
+  }
+
+  delete(id: number): Observable<null> {
+    return this.http.delete<null>(`${environment.endpoints}/register/${id}`);
+  }
+
+}
