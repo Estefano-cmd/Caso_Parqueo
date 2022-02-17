b7d6d192e8511f738d8779cacdbdd600c6f66126
Fernando Castillo Torrico <fernandocto.scz@gmail.com>
2022-02-17 17:47:51 -0400

    NEW: add subscription service

---
+++ caso-parqueo/src/app/shared/services/subscription.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Subscription } from '../types/Subscription';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriptionService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Array<Subscription>> {
+    return this.http.get<Array<Subscription>>(`${environment.endpoints}/subscription`);
+  }
+
+  getOne(id: number): Observable<Subscription> {
+    return this.http.get<Subscription>(`${environment.endpoints}/subscription/${id}`);
+  }
+
+  create(data: Subscription): Observable<null> {
+    return this.http.post<null>(`${environment.endpoints}/subscription`, data);
+  }
+
+  update(id: number, data: Partial<Subscription>): Observable<null> {
+    return this.http.patch<null>(`${environment.endpoints}/subscription/${id}`, data);
+  }
+
+  delete(id: number): Observable<null> {
+    return this.http.delete<null>(`${environment.endpoints}/subscription/${id}`);
+  }
+
+}
