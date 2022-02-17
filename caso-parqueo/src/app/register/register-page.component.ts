34d0c040103afee306a5cd8b797ba906750c1014
Fernando Castillo Torrico <fernandocto.scz@gmail.com>
2022-02-17 17:27:43 -0400

    NEW: add search license place

--- caso-parqueo/src/app/register/register-page.component.ts
+++ caso-parqueo/src/app/register/register-page.component.ts
@@ -1,4 +1,11 @@
+import { BehaviorSubject, of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { VehicleService } from './../shared/services/vehicle.service';
+import { FormControl } from '@angular/forms';
+import { Vehicle } from '../shared/types/Vehicle';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { ClientService } from '../shared/services/client.service';
+import { Client } from '../shared/types/Client';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +13,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit {
-
-  constructor() { }
-
+  searchInput = new FormControl();
+  searchStarted = false;
+  constructor(
+    private vehicleService: VehicleService,
+    private clientService: ClientService
+  ) { }
+  data$ = new BehaviorSubject<any>(null);
   ngOnInit(): void {
   }
 
+  toAssign(): void {
+
+  }
+
+  searchLicensePlate(): void {
+    this.vehicleService.getOneByLicensePlate(this.searchInput.value).pipe(
+      tap(() => this.searchStarted = true),
+      switchMap((vehicle: Vehicle) => {
+        if (!vehicle) {
+          return of(null);
+        }
+        return this.clientService.getOne(vehicle.clientId).pipe(
+          map((client: Client) => {
+            return {
+              vehicle,
+              client
+            };
+          })
+        );
+      })
+    ).subscribe((data: any) => {
+      this.data$.next(data);
+    });
+  }
 }
--- caso-parqueo/src/app/register/register.module.ts
+++ caso-parqueo/src/app/register/register.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterPageComponent } from './register-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -11,7 +13,10 @@ import { RegisterPageComponent } from './register-page.component';
   ],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    ReactiveFormsModule,
+    FormsModule,
+    RegisterRoutingModule,
+    MaterialModule
   ]
 })
 export class RegisterModule { }
